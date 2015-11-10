3e2eccd54125174b402e748b51691434287ecb14
Potxoki <potxoki@gmail.com>
2015-11-10 17:21:06 +0100

    mejora del teclado

--- pr02/src/VentanaJuego.java
+++ pr02/src/VentanaJuego.java
@@ -3,6 +3,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.*;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -17,11 +18,13 @@ public class VentanaJuego extends JFrame {
 	MundoJuego miMundo;        // Mundo del juego
 	CocheJuego miCoche;        // Coche del juego
 	MiRunnable miHilo = null;  // Hilo del bucle principal de juego	
-
+	private Boolean[] pulsaciones = new Boolean[4];
+	
 	/** Constructor de la ventana de juego. Crea y devuelve la ventana inicializada
 	 * sin coches dentro
 	 */
 	public VentanaJuego() {
+		Arrays.fill(pulsaciones, Boolean.FALSE);
 		// Liberación de la ventana por defecto al cerrar
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		// Creación contenedores y componentes
@@ -44,6 +47,7 @@ public class VentanaJuego extends JFrame {
 		// Formato de ventana
 		setSize( 1000, 750 );
 		setResizable( false );
+		
 		// Escuchadores de botones
 		bAcelerar.addActionListener( new ActionListener() {
 			@Override
@@ -77,25 +81,65 @@ public class VentanaJuego extends JFrame {
 		// Añadido para que también se gestione por teclado con el KeyListener
 		pPrincipal.addKeyListener( new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent e) {
+			public void keyReleased(KeyEvent e) {
 				switch (e.getKeyCode()) {
 					case KeyEvent.VK_UP: {
-						miCoche.acelera( +5, 1 );
+						pulsaciones[0]= false;
 						break;
 					}
 					case KeyEvent.VK_DOWN: {
-						miCoche.acelera( -5, 1 );
+						pulsaciones[1]= false;
 						break;
 					}
 					case KeyEvent.VK_LEFT: {
-						miCoche.gira( +10 );
+						pulsaciones[2]= false;
 						break;
 					}
 					case KeyEvent.VK_RIGHT: {
-						miCoche.gira( -10 );
+						pulsaciones[3]= false;
 						break;
 					}
 				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP: {
+					pulsaciones[0]= true;
+					break;
+				}
+				case KeyEvent.VK_DOWN: {
+					pulsaciones[1]= true;
+					break;
+				}
+				case KeyEvent.VK_LEFT: {
+					pulsaciones[2]= true;
+					break;
+				}
+				case KeyEvent.VK_RIGHT: {
+					pulsaciones[3]= true;
+					break;
+				}
+			}
+//				switch (e.getKeyCode()) {
+//					case KeyEvent.VK_UP: {
+//						miCoche.acelera( +5, 1 );
+//						break;
+//					}
+//					case KeyEvent.VK_DOWN: {
+//						miCoche.acelera( -5, 1 );
+//						break;
+//					}
+//					case KeyEvent.VK_LEFT: {
+//						miCoche.gira( +10 );
+//						break;
+//					}
+//					case KeyEvent.VK_RIGHT: {
+//						miCoche.gira( -10 );
+//						break;
+//					}
+//				}
 			}
 		});
 		pPrincipal.setFocusable(true);
@@ -151,6 +195,18 @@ public class VentanaJuego extends JFrame {
 		public void run() {
 			// Bucle principal forever hasta que se pare el juego...
 			while (sigo) {
+				if (pulsaciones[0]){
+					miCoche.acelera( +5, 1 );
+				}
+				if (pulsaciones[1]){
+					miCoche.acelera( -5, 1 );
+				}
+				if (pulsaciones[2]){
+					miCoche.gira( +10 );
+				}
+				if (pulsaciones[3]){
+					miCoche.gira( -10 );
+				}
 				// Mover coche
 				miCoche.mueve( 0.040 );
 				// Chequear choques
